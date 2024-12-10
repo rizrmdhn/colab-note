c5e7fe74fa38b8ea4c3cafbe83bec38c9210b6c3
rizrmdhn <114452320+rizrmdhn@users.noreply.github.com>
2024-12-10 09:21:41 +0800

    feat: Update collaborator page to use new collaborator form and implement session validation

--- src/app/app/notes/[noteId]/collaborator/new/page.tsx
+++ src/app/app/notes/[noteId]/collaborator/new/page.tsx
@@ -1,10 +1,23 @@
-import CreateNewNoteForm from "@/components/create-new-note-form";
+import CreateNewNoteCollaboratorForm from "@/components/create-new-collaborator-form";
+import { getCurrentSession } from "@/lib/session";
+import { redirect } from "next/navigation";
 
-export default function Page() {
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ noteId: string }>;
+}) {
+  const { user } = await getCurrentSession();
+  const noteId = (await params).noteId;
+
+  if (!user) {
+    redirect("/");
+  }
+
   return (
     <div className="flex flex-col items-center justify-center gap-4 xl:p-4">
       <h1 className="self-start text-3xl font-bold">Add a collaborator</h1>
-      <CreateNewNoteForm />
+      <CreateNewNoteCollaboratorForm noteId={noteId} userId={user.id} />
     </div>
   );
 }
